2ba4bc756cf03d808f1af639723299d42b3018ba
Brian Dickson <brianlawrencedickson@gmail.com>
2022-11-10 16:37:29 +0000

    homwork day 2 commits

---
+++ day_2/homework_day_2b.py
@@ -0,0 +1,99 @@
+users = {
+   "Jonathan": {
+     "twitter": "jonnyt",
+     "lottery_numbers": [6, 12, 49, 33, 45, 20],
+     "home_town": "Stirling",
+     "pets": [
+     {
+       "name": "fluffy",
+       "species": "cat"
+     },
+     {
+       "name": "fido",
+       "species": "dog"
+     },
+     {
+       "name": "spike",
+       "species": "dog"
+     }
+   ]
+   },
+   "Erik": {
+     "twitter": "eriksf",
+     "lottery_numbers": [18, 34, 8, 11, 24],
+     "home_town": "Linlithgow",
+     "pets": [
+     {
+       "name": "nemo",
+       "species": "fish"
+     },
+     {
+       "name": "kevin",
+       "species": "fish"
+     },
+     {
+       "name": "spike",
+       "species": "dog"
+     },
+     {
+       "name": "rupert",
+       "species": "parrot"
+     }
+   ]
+   },
+   "Avril": {
+     "twitter": "bridgpally",
+     "lottery_numbers": [12, 14, 33, 38, 9, 25],
+     "home_town": "Dunbar",
+     "pets": [
+       {
+         "name": "monty",
+         "species": "snake"
+       }
+     ]
+   }
+ }
+
+ # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+print(users ["Jonathan"] ["twitter"])
+
+ # 2. Get Erik's hometown
+print(users ["Erik"] ["home_town"])
+
+ # 3. Get the list of Erik's lottery numbers
+print(users ["Erik"] ["lottery_numbers"])
+
+ # 4. Get the species of Avril's pet Monty
+ print(users["Avril"]["pets"][0]["species"])
+
+# print(users ["Avril"] ["pets"] ["species"])
+sorted(users["Erik"]["lottery_numbers"]{0})
+
+ # 5. Get the smallest of Erik's lottery numbers
+sorted(users["Erik"]["lottery_numbers"]{0})
+
+
+# print(users[Erik] [lottery_numbers] [4])
+    
+
+
+ # 6. Return an list of Avril's lottery numbers that are even
+ even_numbers = []
+for number in users["Avril"]["lottery_numbers"]:
+    if number % 2 == 0
+        even_numbers.append(number)
+print(even_numbers)    
+ # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+ users["Erik"]["lottert_numbers"].append(7)
+
+ # 8. Change Erik's hometown to Edinburgh
+ users["Erik"]["home_town" = "edinburgh"]
+
+ # 9. Add a pet dog to Erik called "fluffy"
+new_pet = {
+    "name" : "Fluffy"
+    "species" : "dog"
+}
+users["Erik"]["pets"].append(new_pet)
+
+ # 10. Add another person to the users dictionary
